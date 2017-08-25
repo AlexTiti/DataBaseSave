535a808a6121afb16bf20bdbdbd9f7ab759d1bd1
Administrator <18238818283@sina.cn>
2017-08-25 20:39:43 +0800

    wee commit

---
+++ app/src/main/java/com/example/administrator/databasesave/wee.java
@@ -0,0 +1,13 @@
+package com.example.administrator.databasesave;
+
+/**
+ * <pre>
+ *
+ *   author   :   Alex
+ *   e_mail   :   18238818283@sina.cn
+ *   timr     :   2017/08/25
+ *   desc     :
+ *   version  :   V 1.0.5
+ */
+public class wee {
+}
